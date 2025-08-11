2161e0f55dc61cf7b47c177012a75be2d9050c54
David <dmalesd@yahoo.com>
2025-08-11 20:41:09 +0100

    Add messenger service

---
+++ MauiApp2/Services/Contracts/IMessengerService.cs
@@ -0,0 +1,79 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+
+namespace MauiApp2.Services.Contracts
+{
+    public interface IMessengerService
+    {
+        /// <summary>
+        /// Checks if the message is registered
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <typeparam name="TToken"></typeparam>
+        /// <param name="recipient"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        bool IsRegistered<TMessage, TToken>(Object recipient, TToken token) where TMessage : class where TToken : IEquatable<TToken>;
+
+        /// <summary>
+        /// Checks if the message is registered
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="recipient"></param>
+        /// <returns></returns>
+        bool IsRegistered<TMessage>(Object recipient) where TMessage : class;
+
+        /// <summary>
+        /// Registers the message
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="recipient"></param>
+        /// <param name="handler"></param>
+        void Register<TMessage>(Object recipient, MessageHandler<object, TMessage> handler) where TMessage : class;
+
+        /// <summary>
+        /// Registers the message
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="recipient"></param>
+        /// <param name="token"></param>
+        /// <param name="handler"></param>
+        void Register<TMessage>(Object recipient, string token, MessageHandler<object, TMessage> handler) where TMessage : class;
+
+        /// <summary>
+        /// Sends the message
+        /// </summary>
+        /// <typeparam name="TMessageType"></typeparam>
+        /// <param name="testMessage"></param>
+        void Send<TMessageType>(TMessageType testMessage) where TMessageType : class;
+
+        /// <summary>
+        /// Sends the message
+        /// </summary>
+        /// <typeparam name="TMessageType"></typeparam>
+        /// <param name="testMessage"></param>
+        /// <param name="token"></param>
+        void Send<TMessageType>(TMessageType testMessage, string token) where TMessageType : class;
+
+        /// <summary>
+        /// Unregisters the message
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <typeparam name="TToken"></typeparam>
+        /// <param name="recipient"></param>
+        /// <param name="token"></param>
+        void Unregister<TMessage, TToken>(Object recipient, TToken token) where TMessage : class where TToken : IEquatable<TToken>;
+
+        /// <summary>
+        /// Unregisters the message
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="recipient"></param>
+        void Unregister<TMessage>(Object recipient) where TMessage : class;
+
+        /// <summary>
+        /// Unregisters all messages
+        /// </summary>
+        /// <param name="recipient"></param>
+        void UnregisterAll(Object recipient);
+    }
+}
---
+++ MauiApp2/Services/MessengerService.cs
@@ -0,0 +1,115 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using MauiApp2.Services.Contracts;
+
+
+namespace MauiApp2.Services
+{
+    public class MessengerService : IMessengerService
+    {
+        StrongReferenceMessenger _messenger;
+
+        public MessengerService(IMessenger messenger)
+        {
+            _messenger = (StrongReferenceMessenger)messenger;
+        }
+
+        /// <summary>
+        /// Checks if the message is registered
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <typeparam name="TToken"></typeparam>
+        /// <param name="recipient"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public bool IsRegistered<TMessage, TToken>(Object recipient, TToken token) where TMessage : class where TToken : IEquatable<TToken>
+        {
+            return StrongReferenceMessenger.Default.IsRegistered<TMessage, TToken>(recipient, token);
+        }
+
+        /// <summary>
+        /// Checks if the message is registered
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="recipient"></param>
+        /// <returns></returns>
+        public bool IsRegistered<TMessage>(Object recipient) where TMessage : class
+        {
+            return StrongReferenceMessenger.Default.IsRegistered<TMessage>(recipient);
+        }
+
+        /// <summary>
+        /// Checks if the message is registered
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="recipient"></param>
+        /// <param name="handler"></param>
+        public void Register<TMessage>(Object recipient, MessageHandler<object, TMessage> handler) where TMessage : class
+        {
+            StrongReferenceMessenger.Default.Register(recipient, handler);
+        }
+
+        /// <summary>
+        /// Registers the message
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="recipient"></param>
+        /// <param name="token"></param>
+        /// <param name="handler"></param>
+        public void Register<TMessage>(Object recipient, string token, MessageHandler<object, TMessage> handler) where TMessage : class
+        {
+            StrongReferenceMessenger.Default.Register(recipient, token, handler);
+        }
+
+        /// <summary>
+        /// Sends the message
+        /// </summary>
+        /// <typeparam name="TMessageType"></typeparam>
+        /// <param name="message"></param>
+        public void Send<TMessageType>(TMessageType message) where TMessageType : class
+        {
+            StrongReferenceMessenger.Default.Send(message);
+        }
+
+        /// <summary>
+        /// Sends the message
+        /// </summary>
+        /// <typeparam name="TMessageType"></typeparam>
+        /// <param name="message"></param>
+        /// <param name="token"></param>
+        public void Send<TMessageType>(TMessageType message, string token) where TMessageType : class
+        {
+            StrongReferenceMessenger.Default.Send(message, token);
+        }
+
+        /// <summary>
+        /// Unregisters the message
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <typeparam name="TToken"></typeparam>
+        /// <param name="recipient"></param>
+        /// <param name="token"></param>
+        public void Unregister<TMessage, TToken>(Object recipient, TToken token) where TMessage : class where TToken : IEquatable<TToken>
+        {
+            StrongReferenceMessenger.Default.Unregister<TMessage, TToken>(recipient, token);
+        }
+
+        /// <summary>
+        /// Unregisters the message
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="recipient"></param>
+        public void Unregister<TMessage>(Object recipient) where TMessage : class
+        {
+            StrongReferenceMessenger.Default.Unregister<TMessage>(recipient);
+        }
+
+        /// <summary>
+        /// Unregisters all messages
+        /// </summary>
+        /// <param name="recipient"></param>
+        public void UnregisterAll(Object recipient)
+        {
+            StrongReferenceMessenger.Default.UnregisterAll(recipient);
+        }
+    }
+}
